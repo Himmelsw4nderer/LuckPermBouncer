10684a87e4dcef45c04941b3e1500e6c86902e4e
Himmelsw4nderer <nils.baxheinrich@gmx.de>
2022-06-16 20:17:09 +0200

    init

---
+++ src/main/java/de/himmelswanderer/luckpermbouncer/LuckPermBouncer.java
@@ -0,0 +1,17 @@
+package de.himmelswanderer.luckpermbouncer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LuckPermBouncer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+    //On join Event
+
+}
