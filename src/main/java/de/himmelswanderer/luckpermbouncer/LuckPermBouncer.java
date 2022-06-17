e4cc4f5ea6241263881fec245403f654fc1414b6
Himmelsw4nderer <nils.baxheinrich@gmx.de>
2022-06-17 02:19:09 +0200

    1.0-Snapshot_2 fixed that nothing worked + you can decide if players should be kicked on the config

--- src/main/java/de/himmelswanderer/luckpermbouncer/LuckPermBouncer.java
+++ src/main/java/de/himmelswanderer/luckpermbouncer/LuckPermBouncer.java
@@ -3,14 +3,22 @@ package de.himmelswanderer.luckpermbouncer;
 import de.himmelswanderer.luckpermbouncer.listeners.PlayerJoinListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
 public final class LuckPermBouncer extends JavaPlugin {
 
-    //On Enable
     @Override
     public void onEnable() {
+        //init logger
+        Logger log = getLogger();
+        //config startup
+        getConfig().options().copyDefaults();
+        saveDefaultConfig();
         //register the PlayerJoinListener
-        getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
-        //log
-        System.out.println("started!");
+        getServer().getPluginManager().registerEvents(new PlayerJoinListener(this), this);
+        //log that the plugin is enabled
+        log.log(new LogRecord(Level.INFO, "LuckPermBouncer is enabled"));
     }
 }
--- src/main/java/de/himmelswanderer/luckpermbouncer/listeners/PlayerJoinListener.java
+++ src/main/java/de/himmelswanderer/luckpermbouncer/listeners/PlayerJoinListener.java
@@ -1,17 +1,32 @@
 package de.himmelswanderer.luckpermbouncer.listeners;
 
+import de.himmelswanderer.luckpermbouncer.LuckPermBouncer;
+import net.kyori.adventure.text.Component;
 import org.bukkit.Server;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
 
 import java.util.Collection;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
 
 public class PlayerJoinListener implements Listener {
 
+    private final Logger log;
+    private final FileConfiguration config;
+
+    public PlayerJoinListener(LuckPermBouncer plugin){
+        //load config
+        config = plugin.getConfig();
+        log = plugin.getLogger();
+    }
+
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event){
+    public void onPlayerLogin(PlayerLoginEvent event){
         Player player = event.getPlayer();
         Server server = player.getServer();
         Collection<? extends Player> onlinePlayers = server.getOnlinePlayers();
@@ -19,13 +34,20 @@ public class PlayerJoinListener implements Listener {
         if(server.getMaxPlayers() <= onlinePlayers.size()){
             //if player is permitted to join when full
             if(player.hasPermission("luckpermbouncer.preferred")){
-                //for each online player
-                for(Player onlinePlayer : onlinePlayers){
-                    //if they dont have permission
-                    if(!onlinePlayer.hasPermission("luckpermbouncer.preferred")){
-                        onlinePlayer.kick();
-                        System.out.println("kicked " + onlinePlayer.getName() + "with id: " + onlinePlayer.getUniqueId() + "for "+ player.getName() + "with id: " + player.getUniqueId());
-                        break;
+                //allow the event
+                event.allow();
+                log.log(new LogRecord(Level.INFO ,"joined "+ player.getName() + "with id: " + player.getUniqueId()));
+                //if config is set to kick players
+                if(config.getBoolean("kick-on-full")) {
+                    //for each online player
+                    for (Player onlinePlayer : onlinePlayers) {
+                        //if they don't have permission
+                        if (!onlinePlayer.hasPermission("luckpermbouncer.preferred")) {
+                            //kick player with message You are kicked because the server is full and a privileged player joined
+                            onlinePlayer.kick(Component.text("You are kicked because the server is full and a privileged player joined"));
+                            log.log(new LogRecord(Level.INFO, "kicked" + onlinePlayer.getName() + "with id: " + onlinePlayer.getUniqueId()));
+                            break;
+                        }
                     }
                 }
             }
